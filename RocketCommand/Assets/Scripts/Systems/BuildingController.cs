a5a61374a61b1af9ab1c9a5f7347862e989af3d2
DanielSleczka <danielsleczkads@gmail.com>
2022-05-22 17:30:33 +0200

    building controller added

--- RocketCommand/Assets/Scripts/Systems/BuildingController.cs
+++ RocketCommand/Assets/Scripts/Systems/BuildingController.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BuildingController : MonoBehaviour
 {
     [SerializeField] private List<Building> listOfBuildings;
+    [SerializeField] private Shield shield;
+    [SerializeField] private Transform shieldPosition;
 
+    private bool canCreateNewShield;
 
+    public void InitializeController()
+    {
+        CreateShield();
+    }
 
+    public void UpdateController()
+    {
+        if(shield.IsDestroyed)
+        {
+            canCreateNewShield = true;
+        }
+    }
 
+    public void ReactivatingShield()
+    {
+        if (canCreateNewShield)
+        {
+            CreateShield();
+        }
+    }
 
+    public void CreateShield()
+    {
+        Shield newShield = Instantiate(shield, shieldPosition);
+        shield = newShield;
+        canCreateNewShield = false;
+    }
 }
