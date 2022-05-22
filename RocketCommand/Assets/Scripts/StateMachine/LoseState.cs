7cca28e9fa4205404923eb0158dc7906f4dffc3a
DanielSleczka <danielsleczkads@gmail.com>
2022-05-22 17:29:39 +0200

    created lose state

--- RocketCommand/Assets/Scripts/StateMachine/LoseState.cs
+++ RocketCommand/Assets/Scripts/StateMachine/LoseState.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class LoseState : BaseState
 {
-    private LoseView loseView;
     private ScoreSystem scoreSystem;
     private SaveSystem saveSystem;
+    private LoadingSystem loadingSystem;
+    private LoadingView loadingView;
+    private LoseView loseView;
 
-    public LoseState(LoseView loseView, ScoreSystem scoreSystem, SaveSystem saveSystem)
+    public LoseState(ScoreSystem scoreSystem, SaveSystem saveSystem, LoadingSystem loadingSystem, LoadingView loadingView, LoseView loseView)
     {
-        this.loseView = loseView;
         this.scoreSystem = scoreSystem;
         this.saveSystem = saveSystem;
+        this.loadingSystem = loadingSystem;
+        this.loadingView = loadingView;
+        this.loseView = loseView;
     }
 
 
     public override void InitializeState()
     {
         base.InitializeState();
+        loseView.ShowView();
+        loseView.OnRestartGameButtonClicked_AddListener(ResetLevel);
+        loseView.OnExitButtonClicked_AddListener(ReturnToMainMenu);
     }
 
     public override void UpdateState()
@@ -30,4 +37,20 @@ public class LoseState : BaseState
     {
         base.DestroyState();
     }
+
+    private void ResetLevel()
+    {
+        loadingView.ShowView();
+        loadingSystem.StartLoadingScene(1);
+    }
+    private void ReturnToMainMenu()
+    {
+        loadingView.ShowView();
+        loadingSystem.StartLoadingScene(0);
+    }
+
+
+
+
+
 }
